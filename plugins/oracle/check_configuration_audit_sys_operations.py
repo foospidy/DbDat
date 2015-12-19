52cbedcec9be73d7641811e8522e0b39027b4c2c
foospidy <phillip@foospidy.com>
2015-12-19 09:29:49 -0500

    oracle audit sys operations update

--- plugins/oracle/check_configuration_audit_sys_operations.py
+++ plugins/oracle/check_configuration_audit_sys_operations.py
@@ -4,7 +4,7 @@ class check_configuration_audit_sys_operations():
 	"""
 	# References:
 
-	TITLE    = 'Version Check'
+	TITLE    = 'Audit SYS Operations'
 	CATEGORY = 'Configuration'
 	TYPE     = 'sql'
 	SQL    	 = "SELECT value FROM v$parameter WHERE name='audit_sys_operations'"
@@ -15,16 +15,17 @@ class check_configuration_audit_sys_operations():
 	
 	def do_check(self, *rows):
 		output       = ''
-		
 		for row in rows:
-            #todo
-				
-		
-		
+			if 'FALSE' == row[0][0]:
+				self.result['level'] = 'RED'
+				output += 'Audit of sys operations is not enabled.'
+			else:
+				self.result['level'] = 'GREEN'
+				output += 'Audit of sys operations is enabled.'
+
+		self.result['output'] = output
 			
-			self.result['output'] = output
-			
-			return self.result
-	
+		return self.result
+
 	def __init__(self, parent):
 		print('Performing check: ' + self.TITLE)
