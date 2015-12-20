ff587f67f9ca5db9602332c0f7d61503b3297fda
foospidy <phillip@foospidy.com>
2015-12-19 20:54:17 -0500

    oracle admin restrictions oracle

--- plugins/oracle/__init__.py
+++ plugins/oracle/__init__.py
@@ -4,6 +4,7 @@ from check_configuration_audit_trail import *
 from check_configuration_dictionary_accessibility import *
 from check_configuration_remote_os_authentication import *
 from check_configuration_local_os_authentication import *
+from check_configuration_admin_restrictions_listener import *
 from check_user_failed_logins import *
 from check_user_default_credentials import *
 from check_user_weak_password import *
---
+++ plugins/oracle/check_configuration_admin_restrictions_listener.py
@@ -0,0 +1,41 @@
+import re
+
+class check_configuration_admin_restrictions_listener():
+	"""
+	check_configuration_admin_restrictions_listener
+	"""
+	# References:
+	# http://docs.oracle.com/cd/B19306_01/network.102/b14266/policies.htm
+
+	TITLE    = 'Admin Restrictions Listener'
+	CATEGORY = 'Configuration'
+	TYPE     = 'configuration_file'
+	SQL    	 = None
+	
+	verbose = False
+	skip	= False
+	result  = {}
+	
+	def do_check(self, configuration_file):
+		self.result['level']  = 'RED'
+		self.result['output'] = 'ADMIN_RESTRICTIONS_LISTENER is not enabled.'
+		
+		try:
+			with open(configuration_file) as file:
+				for line in file:
+					if not line.strip().startswith('#'):
+						if re.match('^ADMIN_RESTRICTIONS_LISTENER\s*?=\s*?ON$', line.strip()):
+							self.result['level']  = 'GREEN'
+							self.result['output'] = 'ADMIN_RESTRICTIONS_LISTENER is enabled.'
+			
+		except Exception as e:
+			if self.verbose:
+				print('Cannot read configuration file:\n' + str(e))
+		
+		print self.result['level']
+		return self.result
+	
+	def __init__(self, parent):
+		print('Performing check: ' + self.TITLE)
+		
+		self.verbose = parent.verbose
