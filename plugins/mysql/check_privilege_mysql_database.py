507d6f7c520c4cc0f2c6de215a26b1535cfb5d89
foospidy <phillip@foospidy.com>
2015-12-27 21:35:05 -0500

    spacing fix

--- plugins/mysql/check_privilege_mysql_database.py
+++ plugins/mysql/check_privilege_mysql_database.py
@@ -1,36 +1,36 @@
 class check_privilege_mysql_database():
-	"""
-	check_privilege_mysql_database:
+    """
+    check_privilege_mysql_database:
     Non Admin database users should not have access to the "mysql" database.
-	"""
-	# References:
-	# https://benchmarks.cisecurity.org/downloads/show-single/index.cfm?file=mysql.102
+    """
+    # References:
+    # https://benchmarks.cisecurity.org/downloads/show-single/index.cfm?file=mysql.102
 
-	TITLE    = 'mysql Database Access'
-	CATEGORY = 'Privilege'
-	TYPE     = 'sql'
-	SQL      = "SELECT user, host FROM mysql.db WHERE db='mysql' AND ((Select_priv = 'Y') OR (Insert_priv = 'Y') OR (Update_priv = 'Y') OR (Delete_priv = 'Y') OR (Create_priv = 'Y') OR (Drop_priv = 'Y'))"
-	
-	verbose = False
-	skip	= False
-	result  = {}
+    TITLE    = 'mysql Database Access'
+    CATEGORY = 'Privilege'
+    TYPE     = 'sql'
+    SQL      = "SELECT user, host FROM mysql.db WHERE db='mysql' AND ((Select_priv = 'Y') OR (Insert_priv = 'Y') OR (Update_priv = 'Y') OR (Delete_priv = 'Y') OR (Create_priv = 'Y') OR (Drop_priv = 'Y'))"
 
-	def do_check(self, *rows):
-		if not self.skip:
-			output               = ''
-			self.result['level'] = 'GREEN'
-			
-			for row in rows:
-				for r in row:					
-					self.result['level'] = 'RED'
-					output += r[0] + '\t' + r[1] + '\n'
-			
+    verbose = False
+    skip	= False
+    result  = {}
+
+    def do_check(self, *rows):
+        if not self.skip:
+            output               = ''
+            self.result['level'] = 'GREEN'
+            
+            for row in rows:
+                for r in row:					
+                    self.result['level'] = 'RED'
+                    output += r[0] + '\t' + r[1] + '\n'
+
             if 'GREEN' == self.result['level']:
                 output = 'No users found with privileges to the mysql database.'
-            
-			self.result['output'] = output
-		
-		return self.result
+
+            self.result['output'] = output
+
+        return self.result
 	
-	def __init__(self, parent):
-		print('Performing check: ' + self.TITLE)
+    def __init__(self, parent):
+        print('Performing check: ' + self.TITLE)
