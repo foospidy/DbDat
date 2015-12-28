68d9b5e2c68ebdd04baabb1e2f81d4fc296db907
foospidy <phillip@foospidy.com>
2015-12-28 15:47:05 -0500

    mysql updates

---
+++ plugins/mysql/check_configuration_skip_symbolic_links.py
@@ -0,0 +1,32 @@
+class check_configuration_skip_symbolic_links():
+	"""
+	check_configuration_skip_symbolic_links:
+    Prevents sym links being used for data base files. This is especially
+    important when MySQL is executing as root as arbitrary files may be overwritten.
+	"""
+	# References:
+    # https://benchmarks.cisecurity.org/downloads/show-single/index.cfm?file=mysql.102
+
+	TITLE    = Skip Symbolic Links'
+	CATEGORY = 'Configuration'
+	TYPE     = 'sql'
+	SQL    	 = "SHOW GLOBAL VARIABLES LIKE 'have_symlink'"
+	
+	verbose = False
+	skip	= False
+	result  = {}
+	
+	def do_check(self, *rows):		
+		for row in rows:
+			for r in row:
+				if 'DISABLED' != r[1]:
+					self.result['level']  = 'RED'
+					self.result['output'] = 'Symlinks is enabled.'
+				else:
+					self.result['level']  = 'GREEN'
+					self.result['output'] = 'Symlinks is disabled.'
+			
+			return self.result
+	
+	def __init__(self, parent):
+		print('Performing check: ' + self.TITLE)
