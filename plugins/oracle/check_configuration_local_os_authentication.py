63546c93e7371c878981eae733c4e5a4903e869c
foospidy <phillip@foospidy.com>
2015-12-30 08:06:52 -0500

    code clean up

--- plugins/oracle/check_configuration_local_os_authentication.py
+++ plugins/oracle/check_configuration_local_os_authentication.py
@@ -1,6 +1,8 @@
 class check_configuration_local_os_authentication():
 	"""
 	check_configuration_local_os_authentication
+	If the os_authent_prefix is "" (Null), then the OS Authenticated accounts
+	cannot log in using the password
 	"""
 	# References:
 	# http://www.dba-oracle.com/security/local_os_authentication.htm
@@ -8,24 +10,23 @@ class check_configuration_local_os_authentication():
 	TITLE    = 'Local OS Authentication'
 	CATEGORY = 'Configuration'
 	TYPE     = 'sql'
-	SQL    	 = "SELECT value FROM v$parameter WHERE name='os_authent_prefix'"
+	SQL    	 = "SELECT UPPER(value) FROM v$parameter WHERE UPPER(name)='OS_AUTHENT_PREFIX'"
 	
 	verbose = False
 	skip	= False
 	result  = {}
 	
-	def do_check(self, *rows):
-		output       = ''
-		for row in rows:
-			if '' != str(row[0][0]):
-				self.result['level'] = 'RED'
-				output += 'OS authentication prefix is not null. Set os_authent_prefix to null.'
-			else:
-				self.result['level'] = 'GREEN'
-				output += 'OS authentication prefix is null.'
+	def do_check(self, *results):
 
-		self.result['output'] = output
-			
+		for rows in results:
+			for row in rows:
+				if '' != str(row[0]):
+					self.result['level']  = 'RED'
+					self.result['output'] = 'OS authentication prefix is (%s) enabled. Set os_authent_prefix to null.' % (row[0])
+				else:
+					self.result['level']  = 'GREEN'
+					self.result['output'] = 'OS authentication prefix is "(%s)" not enabled.' % (row[0])
+
 		return self.result
 
 	def __init__(self, parent):
