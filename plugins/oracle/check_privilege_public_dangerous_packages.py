6bde87d00b7b9784cae42cfba0157fd78ea0d6fa
foospidy <phillip@foospidy.com>
2016-01-03 00:15:14 -0500

    oracle updates

--- plugins/oracle/check_privilege_public_dangerous_packages.py
+++ plugins/oracle/check_privilege_public_dangerous_packages.py
@@ -1,11 +1,12 @@
 class check_privilege_public_dangerous_packages():
 	"""
 	check_privilege_public_dangerous_packages
-	Revoke execute privilege from PUBLIC on these ojbects.
+	Revoke execute privilege from PUBLIC on "dangerous" packages.
 	"""
 	# References:
 	# http://www.red-database-security.com/wp/sentrigo_webinar.pdf
     # http://blog.opensecurityresearch.com/2012/03/top-10-oracle-steps-to-secure-oracle.html
+    # https://benchmarks.cisecurity.org/downloads/show-single/?file=oracle11gR2.210
 
 	TITLE    = 'PUBLIC Dangerous Packages'
 	CATEGORY = 'Privilege'
@@ -23,25 +24,43 @@ class check_privilege_public_dangerous_packages():
 		'UTL_SMTP',
 		'UTL_TCP',
 		'UTL_DBWS',
+        'UTL_MAIL',
+        'UTL_ORAMTS',
 		'DBMS_SQL',
 		'DBMS_LOB',
 		'DBMS_XMLGEN',
 		'DBMS_AW_XML',
 		'DBMS_RANDOM',
 		'DBMS_OBFUSCATION_TOOLKIT',	
-		'dbms_advisor',
-		'dbms_ldap',
-		'dbms_ldap_utl',
-		'dbms_job',
-		'dbms_scheduler',
-		'dbms_ddl',
-		'dbms_epg',
-		'dbms_xmlgen',
-		'dbms_aw_xml',
+		'DBMS_ADVISOR',
+		'DBMS_LDAP',
+		'DBMS_LDAP_UTL',
+		'DBMS_JOB',
+		'DBMS_SCHEDULER',
+		'DBMS_DDL',
+		'DBMS_EPG',
+        'DBMS_CRYPTO',
+        'DBMS_JAVA',
+        'DBMS_JAVA_TEST',
+        'DBMS_BACKUP_RESTORE',
+        'DBMS_XMLQUERY',
+        'DBMS_SYS_SQL',
+        'DBMS_AQADM_SYSCALLS',
+        'DBMS_AQADM_SYS',
+        'DBMS_REPACT_SQL_UTL',
+        'DBMS_STREAMS_ADM_UTL',
+        'DBMS_STREAMS_RPC',
+        'DBMS_PRVTAQIM',
+        'DBMS_IJOB',
+        'DBMS_FILE_TRANSFER',
+        'INITJVMAUX',
 		'CTXSYS.DRITHSX',
 		'ORDSYS.ORD_DICOM',
 		'KUPP$PROC',
-		'HTTPURITYPE'
+		'HTTPURITYPE',
+        'LTADM',
+        'WWV_DBMS_SQL',
+        'WWV_EXECUTE_IMMEDIATE'
 		}
 	
 	def do_check(self, *results):
@@ -63,5 +82,5 @@ class check_privilege_public_dangerous_packages():
 	def __init__(self, parent):
 		print('Performing check: ' + self.TITLE)
 
-		self.SQL  = "SELECT table_name FROM sys.dba_tab_privs WHERE grantee='PUBLIC' AND privilege='EXECUTE' "
-		self.SQL += "AND table_name IN ('" + "', '".join(self.dangerous_packages) + "')"
+		self.SQL  = "SELECT UPPER(table_name) FROM sys.dba_tab_privs WHERE grantee='PUBLIC' AND privilege='EXECUTE' "
+		self.SQL += "AND UPPER(table_name) IN ('" + "', '".join(self.dangerous_packages) + "')"
