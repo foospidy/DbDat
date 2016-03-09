017bc6b534bd8c2a1913eca96a5bb30d5c7c3c4c
Phillip Maddux II <foospidy@enterprise.foospidy.com>
2016-03-08 19:07:40 -0500

    mssql updates

---
+++ plugins/mssql/check_configuration_database_audit_specifications.py
@@ -0,0 +1,34 @@
+class check_configuration_database_audit_specifications():
+    """
+    check_configuration_database_audit_specifications:
+    Auditing of defined database level events. Review to ensure proper auditing
+    is enabled.
+    """
+    # References:
+    # https://blog.netspi.com/sql-server-persistence-part-1-startup-stored-procedures/
+
+    TITLE    = 'Database Audit Specifications'
+    CATEGORY = 'Configuration'
+    TYPE     = 'sql'
+    SQL      = "SELECT * FROM sys.server_audits AS a JOIN sys.database_audit_specifications AS s ON a.audit_guid = s.audit_guid JOIN sys.database_audit_specification_details AS d ON s.database_specification_id = d.database_specification_id"
+
+    verbose = False
+    skip    = False
+    result  = {}
+
+    def do_check(self, *results):
+        
+        for rows in results:
+            if 0 == len(rows):
+                self.result['level']  = 'RED'
+                self.result['output'] = 'There are no database audit specifications enabled.'
+                
+            else:
+               for row in rows:
+                   self.result['level']  = 'YELLOW'
+                   self.result['output'] = '%s %s %s \n' % (row[0], row[1])
+
+        return self.result
+
+    def __init__(self, parent):
+        print('Performing check: ' + self.TITLE)
---
+++ plugins/mssql/check_configuration_server_audit_specifications.py
@@ -0,0 +1,34 @@
+class check_configuration_server_audit_specifications():
+    """
+    check_configuration_server_audit_specifications:
+    Auditing of defined server level events. Review to ensure proper auditing
+    is enabled.
+    """
+    # References:
+    # https://benchmarks.cisecurity.org/downloads/show-single/index.cfm?file=sql2012DB.120
+
+    TITLE    = 'Server Audit Specifications'
+    CATEGORY = 'Configuration'
+    TYPE     = 'sql'
+    SQL      = "SELECT * FROM sys.server_audits AS a JOIN sys.server_audit_specifications AS s ON a.audit_guid = s.audit_guid JOIN sys.server_audit_specification_details AS d ON s.server_specification_id = d.server_specification_id"
+
+    verbose = False
+    skip    = False
+    result  = {}
+
+    def do_check(self, *results):
+        
+        for rows in results:
+            if 0 == len(rows):
+                self.result['level']  = 'RED'
+                self.result['output'] = 'There are no server audit specifications enabled.'
+                
+            else:
+               for row in rows:
+                   self.result['level']  = 'YELLOW'
+                   self.result['output'] = '%s %s %s \n' % (row[0], row[1])
+
+        return self.result
+
+    def __init__(self, parent):
+        print('Performing check: ' + self.TITLE)
