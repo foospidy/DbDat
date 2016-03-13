9a7c509d1a0cadf912cac2feb97afbfa16020473
foospidy <phillip@foospidy.com>
2016-03-13 07:54:38 -0400

    mssql update

--- plugins/mssql/check_configuration_database_audit_specifications.py
+++ plugins/mssql/check_configuration_database_audit_specifications.py
@@ -6,11 +6,28 @@ class check_configuration_database_audit_specifications():
     """
     # References:
     # https://blog.netspi.com/sql-server-persistence-part-1-startup-stored-procedures/
+    # https://gist.github.com/nullbind/5da8b5113da007ba0111
 
     TITLE    = 'Database Audit Specifications'
     CATEGORY = 'Configuration'
     TYPE     = 'sql'
-    SQL      = "SELECT * FROM sys.server_audits AS a JOIN sys.database_audit_specifications AS s ON a.audit_guid = s.audit_guid JOIN sys.database_audit_specification_details AS d ON s.database_specification_id = d.database_specification_id"
+    SQL      = """
+                SELECT	a.audit_id,
+                a.name as audit_name,
+                s.name as database_specification_name,
+                d.audit_action_name,
+                s.is_state_enabled,
+                d.is_group,
+                s.create_date,
+                s.modify_date,
+                d.audited_result
+                FROM sys.server_audits AS a
+                JOIN sys.database_audit_specifications AS s
+                ON a.audit_guid = s.audit_guid
+                JOIN sys.database_audit_specification_details AS d
+                ON s.database_specification_id = d.database_specification_id 
+                WHERE s.is_state_enabled = 1
+                """
 
     verbose = False
     skip    = False
@@ -26,7 +43,7 @@ class check_configuration_database_audit_specifications():
             else:
                for row in rows:
                    self.result['level']  = 'YELLOW'
-                   self.result['output'] = '%s %s %s \n' % (row[0], row[1])
+                   self.result['output'] = '%s %s %s %s \n' % (row[0], row[1], row[2], row[3])
 
         return self.result
 
--- plugins/mssql/check_configuration_server_audit_specifications.py
+++ plugins/mssql/check_configuration_server_audit_specifications.py
@@ -5,12 +5,28 @@ class check_configuration_server_audit_specifications():
     is enabled.
     """
     # References:
-    # https://blog.netspi.com/sql-server-persistence-part-1-startup-stored-procedures/
+    # https://benchmarks.cisecurity.org/downloads/show-single/index.cfm?file=sql2012DB.120
 
     TITLE    = 'Server Audit Specifications'
     CATEGORY = 'Configuration'
     TYPE     = 'sql'
-    SQL      = "SELECT * FROM sys.server_audits AS a JOIN sys.server_audit_specifications AS s ON a.audit_guid = s.audit_guid JOIN sys.server_audit_specification_details AS d ON s.server_specification_id = d.server_specification_id"
+    SQL      = """
+                SELECT audit_id, 
+                a.name as audit_name, 
+                s.name as server_specification_name,
+                d.audit_action_name,
+                s.is_state_enabled,
+                d.is_group,
+                d.audit_action_id,	
+                s.create_date,
+                s.modify_date
+                FROM sys.server_audits AS a
+                JOIN sys.server_audit_specifications AS s
+                ON a.audit_guid = s.audit_guid
+                JOIN sys.server_audit_specification_details AS d
+                ON s.server_specification_id = d.server_specification_id
+                WHERE s.is_state_enabled = 1
+                """
 
     verbose = False
     skip    = False
@@ -26,7 +42,7 @@ class check_configuration_server_audit_specifications():
             else:
                for row in rows:
                    self.result['level']  = 'YELLOW'
-                   self.result['output'] = '%s %s %s \n' % (row[0], row[1])
+                   self.result['output'] = '%s %s %s %s \n' % (row[0], row[1], row[2], row[3])
 
         return self.result
 
